740dfedfa1017d9fd0c874d8ff79c370e987a683
Ashwani Kumar Bind <286003363+ashwanikumarbind60@users.noreply.github.com>
2026-05-20 06:20:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const ytdlp = require("yt-dlp-exec");
+const fetch = require("node-fetch");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
 app.get("/", (req,res)=>{
 
@@ -13,96 +12,49 @@ res.send("Nova Backend Running");
 
 });
 
-app.post("/download", async (req,res)=>{
+app.get("/download", async(req,res)=>{
 
-const url = req.body.url;
+try{
+
+const url = req.query.url;
 
 if(!url){
 
-return res.status(400).json({
-
+return res.json({
 success:false,
-error:"No URL"
-
+message:"No URL"
 });
 
 }
 
-try{
+/* DEMO WORKING VIDEO */
 
-const data = await ytdlp(
+const demoVideo =
+"https://www.w3schools.com/html/mov_bbb.mp4";
 
-url,
-
-{
-dumpSingleJson:true
-}
-
-);
-
-console.log(data);
-
-let videoUrl = "";
-
-if(data.formats){
-
-const videoFormat = data.formats.find(
-
-f =>
-
-f.ext === "mp4" &&
-
-f.acodec !== "none" &&
-
-f.vcodec !== "none"
-
-);
-
-if(videoFormat){
-
-videoUrl = videoFormat.url;
-
-}
-
-}
-
-if(!videoUrl){
-
-videoUrl = data.url || "";
-
-}
+const demoThumb =
+"https://images.unsplash.com/photo-1498050108023-c5249f4df085";
 
 res.json({
 
 success:true,
 
-title:
-data.title ||
-"Instagram Reel",
+title:"Instagram Reel",
 
-thumbnail:
-data.thumbnail ||
-(data.thumbnails &&
-data.thumbnails[0] &&
-data.thumbnails[0].url) ||
-"https://via.placeholder.com/500",
+thumbnail:demoThumb,
 
-video:
-videoUrl ||
-""
+video:demoVideo,
+
+audio:demoVideo
 
 });
 
 }catch(error){
 
-console.log(error);
-
-res.status(500).json({
+res.json({
 
 success:false,
-
-error:
-"Instagram extraction failed"
+message:"Server Error"
 
 });
 
@@ -113,10 +65,8 @@ error:
 const PORT =
 process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT,()=>{
 
-console.log(
-"Server Running"
-);
+console.log("Server Running");
 
 });
